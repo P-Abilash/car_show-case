b33f439820184796ac5489921dfc74a32be6235f
P-Abilash <abilash@mobilelabs.in>
2023-08-07 19:52:17 +0530

    first commit

---
+++ components/ShowMore.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useRouter } from "@/node_modules/next/navigation";
+import { ShowMoreProps } from "@/types/index";
+import { updateSearchParams } from "@/utils/index";
+import CustomButton from "./CustomButton";
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+     const newLimit = (pageNumber + 1) * 10;
+
+     const newPathname = updateSearchParams("limit", `${newLimit}`);
+    
+    router.push(newPathname);
+  }; 
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton title="Show More" btnType="button" containerStyles="bg-primary-blue rounded-full text-white" handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ShowMore;
